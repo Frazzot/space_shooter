9c3dc4a724b15c3403ee6392293f2b8611e1e638
Ida Franzén Karlsson <frazzoot@gmail.com>
2018-03-03 21:07:38 +0100

    Added player and velocity

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import pygame as pg
 
+vec = pg.math.Vector2
+
 class Player:
-    def __init__(self, colour, pos, size, speed_x, speed_y):
+    def __init__(self, colour, pos, size, velocity):
         self.img = pg.Surface(size)
         self.img.fill(colour)
         self.pos = pos
-        self.speed_x = speed_x
-        self.speed_y = speed_y
+        self.vel = velocity
 
     def draw(self):
         screen.blit(self.img, self.pos)
+
+    def update(self, dt):
+        self.keys_pressed()
+        self.pos += self.vel * dt / 1000
+        self.vel -= self.vel * 5 * dt / 1000
+
+    def keys_pressed(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            self.vel.x = -150
+        if keys[pg.K_d]:
+            self.vel.x = 150
+        
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -20,14 +34,19 @@ screen_height = 900
 pg.init()
 screen = pg.display.set_mode([screen_width, screen_height])
 
+player = Player(WHITE, vec(300, 300), (30, 30), vec(0, 0))
+
 running = True
 clock = pg.time.Clock()
+dt = 0
+
 
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-
+    player.update(dt)
     screen.fill(BLACK)
+    player.draw()
     pg.display.flip()
-    clock.tick(60)
+    dt = clock.tick(60)
